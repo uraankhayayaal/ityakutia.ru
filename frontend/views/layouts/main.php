85c2eba8221cadb154289bc2627248180d0f9869
uraankhayayaal <uraankhayayaal@gmail.com>
2018-07-01 00:27:38 +0900

    NEW data

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -45,11 +45,12 @@ AppAsset::register($this);
     (function (d, w, c) {
         (w[c] = w[c] || []).push(function() {
             try {
-                w.yaCounter44427349 = new Ya.Metrika({
+                w.yaCounter44427349 = new Ya.Metrika2({
                     id:44427349,
                     clickmap:true,
                     trackLinks:true,
-                    accurateTrackBounce:true
+                    accurateTrackBounce:true,
+                    webvisor:true
                 });
             } catch(e) { }
         });
@@ -59,12 +60,12 @@ AppAsset::register($this);
             f = function () { n.parentNode.insertBefore(s, n); };
         s.type = "text/javascript";
         s.async = true;
-        s.src = "https://mc.yandex.ru/metrika/watch.js";
+        s.src = "https://mc.yandex.ru/metrika/tag.js";
 
         if (w.opera == "[object Opera]") {
             d.addEventListener("DOMContentLoaded", f, false);
         } else { f(); }
-    })(document, window, "yandex_metrika_callbacks");
+    })(document, window, "yandex_metrika_callbacks2");
 </script>
 <noscript><div><img src="https://mc.yandex.ru/watch/44427349" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
 <!-- /Yandex.Metrika counter -->
