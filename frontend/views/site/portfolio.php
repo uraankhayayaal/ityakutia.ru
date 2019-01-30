b0cde081cdddb41e66e06a5f7d96a86f661328b6
uraankhayayaal <uraankhayayaal@gmail.com>
2019-01-30 12:53:22 +0900

    add medicity

--- frontend/views/site/portfolio.php
+++ frontend/views/site/portfolio.php
@@ -16,8 +16,31 @@ $this->params['breadcrumbs'][] = $this->title;
 	        <div class="col s12 m4 l3">
 	          <div class="card waves-effect waves-block waves-light">
 	            <div class="card-image waves-effect waves-block waves-light">
-	              <?= Html::img("@web/images/logo2children.png", ['class' => 'activator', 'alt' => 'Admin14 разработка сайтов и мобильных приложений - портфолио - Ресторан Тыгын Дархан']); ?>
-	              <!-- <span class="card-title">Ресторан Тыгын Дархан</span> -->
+	              <?= Html::img("@web/images/medicity.png", ['class' => 'activator', 'alt' => 'Admin14 разработка сайтов и мобильных приложений - портфолио - КОРЕЯ ЛАЙФ ЦЕНТР']); ?>
+	              <!-- <span class="card-title">КОРЕЯ ЛАЙФ ЦЕНТР</span> -->
+	            </div>
+	            <div class="card-content">
+	              <span class="card-title activator grey-text text-darken-4">КОРЕЯ ЛАЙФ ЦЕНТР<i class="material-icons right">more_vert</i></span>
+	            </div>
+	            <div class="card-action">
+	              <a href="http://daegulife.ru" target="_blank">Сайт</a>
+	            </div>
+							<div class="card-reveal">
+								<span class="card-title grey-text text-darken-4">КОРЕЯ ЛАЙФ ЦЕНТР<i class="material-icons right">close</i></span>
+								<p>ЛЕЧЕНИЕ И ОБСЛЕДОВАНИЕ В ЮЖНОЙ КОРЕЕ</p>
+								<p>ООО "МЕДИСИТИ" оказывает специализированную медицинскую офтальмологическую помощь населению Республики Саха (Якутия). В том числе первичный прием офтальмолога - проверка остроты зрения, полей зрения, внутриглазного давления, осмотр отделов глаза и глазного дна.</p>
+								<p>Является представительством медицинских учреждений города Тэгу (Южная Корея).</p>
+										<a href="http://daegulife.ru" target="_blank" class="btn right">Перейти на сайт</a>
+							</div>
+	          </div>
+					</div>
+					
+					
+					<div class="col s12 m4 l3">
+	          <div class="card waves-effect waves-block waves-light">
+	            <div class="card-image waves-effect waves-block waves-light">
+	              <?= Html::img("@web/images/logo2children.png", ['class' => 'activator', 'alt' => 'Admin14 разработка сайтов и мобильных приложений - портфолио - Детский образовательный портал']); ?>
+	              <!-- <span class="card-title">Детский образовательный портал</span> -->
 	            </div>
 	            <div class="card-content">
 	              <span class="card-title activator grey-text text-darken-4">Детский образовательный портал<i class="material-icons right">more_vert</i></span>
@@ -25,19 +48,19 @@ $this->params['breadcrumbs'][] = $this->title;
 	            <div class="card-action">
 	              <a href="http://2children.ru" target="_blank">Сайт</a>
 	            </div>
-			    <div class="card-reveal">
-			      <span class="card-title grey-text text-darken-4">Детский образовательный портал<i class="material-icons right">close</i></span>
-			      <p>На детском образовательном портале Якутска 2children.ru родители Якутска могут выбрать для своего ребенка оптимальный по цене, графику работы, расположению подходящий частный детский сад, кружок, секцию или лагерь для детей из всех образовательных организаций республики Саха (Якутия) и записаться онлайн</p>
-	              <a href="http://2children.ru" target="_blank" class="btn right">Перейти на сайт</a>
-			    </div>
+							<div class="card-reveal">
+								<span class="card-title grey-text text-darken-4">Детский образовательный портал<i class="material-icons right">close</i></span>
+								<p>На детском образовательном портале Якутска 2children.ru родители Якутска могут выбрать для своего ребенка оптимальный по цене, графику работы, расположению подходящий частный детский сад, кружок, секцию или лагерь для детей из всех образовательных организаций республики Саха (Якутия) и записаться онлайн</p>
+										<a href="http://2children.ru" target="_blank" class="btn right">Перейти на сайт</a>
+							</div>
 	          </div>
 	        </div>
 
           	<div class="col s12 m4 l3">
 	          <div class="card waves-effect waves-block waves-light">
 	            <div class="card-image waves-effect waves-block waves-light">
-	              <?= Html::img("@web/images/logo_tmdz.png", ['class' => 'activator', 'alt' => 'Admin14 разработка сайтов и мобильных приложений - портфолио - Ресторан Тыгын Дархан']); ?>
-	              <!-- <span class="card-title">Ресторан Тыгын Дархан</span> -->
+	              <?= Html::img("@web/images/logo_tmdz.png", ['class' => 'activator', 'alt' => 'Admin14 разработка сайтов и мобильных приложений - портфолио - Туймаада займы']); ?>
+	              <!-- <span class="card-title">Туймаада займы</span> -->
 	            </div>
 	            <div class="card-content">
 	              <span class="card-title activator grey-text text-darken-4">Туймаада займы<i class="material-icons right">more_vert</i></span>
@@ -45,13 +68,13 @@ $this->params['breadcrumbs'][] = $this->title;
 	            <div class="card-action">
 	              <a rel="nofollow" href="https://tuymaadazaim.ru/" target="_blank">Сайт</a>
 	            </div>
-			    <div class="card-reveal">
-			      <span class="card-title grey-text text-darken-4">Туймаада займы<i class="material-icons right">close</i></span>
-			      <p>ООО МКК «Кыым» работает на рынке финансовых услуг с 19 октября 2016 г.</p>
-			      <p>Нашими клиентами являются больше 1000 человек по всей территории Республики Саха (Якутия).</p>
-			      <p>Наша цель – предоставление финансовых услуг высокого качества и помощь в решении финансовых проблем!</p>
-	              <a rel="nofollow" href="https://tuymaadazaim.ru/" target="_blank" class="btn right">Перейти на сайт</a>
-			    </div>
+							<div class="card-reveal">
+								<span class="card-title grey-text text-darken-4">Туймаада займы<i class="material-icons right">close</i></span>
+								<p>ООО МКК «Кыым» работает на рынке финансовых услуг с 19 октября 2016 г.</p>
+								<p>Нашими клиентами являются больше 1000 человек по всей территории Республики Саха (Якутия).</p>
+								<p>Наша цель – предоставление финансовых услуг высокого качества и помощь в решении финансовых проблем!</p>
+										<a rel="nofollow" href="https://tuymaadazaim.ru/" target="_blank" class="btn right">Перейти на сайт</a>
+							</div>
 	          </div>
 	        </div>
 
@@ -67,12 +90,12 @@ $this->params['breadcrumbs'][] = $this->title;
 	            <div class="card-action">
 	              <a rel="nofollow" href="http://tygyndarhanrest.ru" target="_blank">Сайт</a>
 	            </div>
-			    <div class="card-reveal">
-			      <span class="card-title grey-text text-darken-4">Ресторан Тыгын Дархан<i class="material-icons right">close</i></span>
-			      <p>Ресторан «Тыгын Дархан» проедоставляет блюда национальной якутской, русской и европейской кухни.</p>
-			      <p>Изюминка ресторана - современная интерпретация национальных блюд - возрождение старинных рецептов якутских блюд, адаптированных к современным технологиям</p>
-	              <a rel="nofollow" href="http://tygyndarhanrest.ru" target="_blank" class="btn right">Перейти на сайт</a>
-			    </div>
+							<div class="card-reveal">
+								<span class="card-title grey-text text-darken-4">Ресторан Тыгын Дархан<i class="material-icons right">close</i></span>
+								<p>Ресторан «Тыгын Дархан» проедоставляет блюда национальной якутской, русской и европейской кухни.</p>
+								<p>Изюминка ресторана - современная интерпретация национальных блюд - возрождение старинных рецептов якутских блюд, адаптированных к современным технологиям</p>
+										<a rel="nofollow" href="http://tygyndarhanrest.ru" target="_blank" class="btn right">Перейти на сайт</a>
+							</div>
 	          </div>
 	        </div>
 
@@ -89,10 +112,10 @@ $this->params['breadcrumbs'][] = $this->title;
 	              <a rel="nofollow" href="http://zn.yakutiaventure.ru" target="_blank">Сайт</a>
 	            </div>
 	            <div class="card-reveal">
-			      <span class="card-title grey-text text-darken-4">Золотой наперсток<i class="material-icons right">close</i></span>
-			      <p>Местный стартап который продает носки с национальным дизайном и вся продукция шьется в городе Якутске.</p>
-	              <a rel="nofollow" href="http://zn.yakutiaventure.ru" target="_blank" class="btn right">Перейти на сайт</a>
-			    </div>
+								<span class="card-title grey-text text-darken-4">Золотой наперсток<i class="material-icons right">close</i></span>
+								<p>Местный стартап который продает носки с национальным дизайном и вся продукция шьется в городе Якутске.</p>
+										<a rel="nofollow" href="http://zn.yakutiaventure.ru" target="_blank" class="btn right">Перейти на сайт</a>
+							</div>
 	          </div>
 	        </div>
 
@@ -109,10 +132,10 @@ $this->params['breadcrumbs'][] = $this->title;
 	              <a rel="nofollow" href="https://play.google.com/store/apps/details?id=ru.admin14.dtycard&hl=ru" target="_blank">Мобильное приложение</a>
 	            </div>
 	            <div class="card-reveal">
-			      <span class="card-title grey-text text-darken-4">Картмоне<i class="material-icons right">close</i></span>
-			      <p>Мобильное приложение для сохранения дисконтных карт в смартфоне используя технологии считывания штрих кодов.</p>
-	              <a rel="nofollow" href="https://play.google.com/store/apps/details?id=ru.admin14.dtycard&hl=ru" target="_blank" class="btn right">Перейти на Google Play</a>
-			    </div>
+								<span class="card-title grey-text text-darken-4">Картмоне<i class="material-icons right">close</i></span>
+								<p>Мобильное приложение для сохранения дисконтных карт в смартфоне используя технологии считывания штрих кодов.</p>
+										<a rel="nofollow" href="https://play.google.com/store/apps/details?id=ru.admin14.dtycard&hl=ru" target="_blank" class="btn right">Перейти на Google Play</a>
+							</div>
 	          </div>
 	        </div>
 
@@ -129,10 +152,10 @@ $this->params['breadcrumbs'][] = $this->title;
 	              <a rel="nofollow" href="http://xn--14-dlclb0ed.xn--p1ai/" target="_blank">Сайт</a>
 	            </div>
 	            <div class="card-reveal">
-			      <span class="card-title grey-text text-darken-4">Строительная компания "Север"<i class="material-icons right">close</i></span>
-			      <p>Строительная компания.</p>
-	              <a rel="nofollow" href="http://xn--14-dlclb0ed.xn--p1ai/" target="_blank" class="btn right">Перейти на сайт</a>
-			    </div>
+								<span class="card-title grey-text text-darken-4">Строительная компания "Север"<i class="material-icons right">close</i></span>
+								<p>Строительная компания.</p>
+										<a rel="nofollow" href="http://xn--14-dlclb0ed.xn--p1ai/" target="_blank" class="btn right">Перейти на сайт</a>
+							</div>
 	          </div>
 	        </div>
 
@@ -148,10 +171,10 @@ $this->params['breadcrumbs'][] = $this->title;
 	              <a rel="nofollow" href="http://aic.s-vfu.ru" target="_blank">Сайт</a>
 	            </div>
 	            <div class="card-reveal">
-			      <span class="card-title grey-text text-darken-4">Арктический Инновационный Центр<i class="material-icons right">close</i></span>
-			      <p>Подразделение по развитию инноваций и студенческого предпринимательства СВФУ им М.К.Аммосова</p>
-	              <a rel="nofollow" href="http://aic.s-vfu.ru" target="_blank" class="btn right">Перейти на сайт</a>
-			    </div>
+								<span class="card-title grey-text text-darken-4">Арктический Инновационный Центр<i class="material-icons right">close</i></span>
+								<p>Подразделение по развитию инноваций и студенческого предпринимательства СВФУ им М.К.Аммосова</p>
+										<a rel="nofollow" href="http://aic.s-vfu.ru" target="_blank" class="btn right">Перейти на сайт</a>
+							</div>
 	          </div>
 	        </div>
 
@@ -168,11 +191,11 @@ $this->params['breadcrumbs'][] = $this->title;
 	              <a rel="nofollow" href="http://yakutskgeo.ru/" target="_blank">Сайт</a>
 	            </div>
 	            <div class="card-reveal">
-			      <span class="card-title grey-text text-darken-4">Якутск геология<i class="material-icons right">close</i></span>
-			      <p>одно из крупнейших геологоразведочных учреждений Российской Федерации. Предприятие создано 01 января 2007 года распоряжением Правительства РС(Я) № 1425-р от 01 декабря 2006 года путем реорганизации и слияния геологоразведочных организаций - ГУГГП «Восточно-Якутское», ГУП «Алдангеология», ГУП «Якутская поисково-съемочная экспедиция» и ГУП «Центргеоланалитика».</p>
-			      <p>В настоящий момент, предприятие является собственником ОАО «Янгеология».</p>
-	              <a rel="nofollow" href="http://yakutskgeo.ru/" target="_blank" class="btn right">Перейти на сайт</a>
-			    </div>
+								<span class="card-title grey-text text-darken-4">Якутск геология<i class="material-icons right">close</i></span>
+								<p>одно из крупнейших геологоразведочных учреждений Российской Федерации. Предприятие создано 01 января 2007 года распоряжением Правительства РС(Я) № 1425-р от 01 декабря 2006 года путем реорганизации и слияния геологоразведочных организаций - ГУГГП «Восточно-Якутское», ГУП «Алдангеология», ГУП «Якутская поисково-съемочная экспедиция» и ГУП «Центргеоланалитика».</p>
+								<p>В настоящий момент, предприятие является собственником ОАО «Янгеология».</p>
+										<a rel="nofollow" href="http://yakutskgeo.ru/" target="_blank" class="btn right">Перейти на сайт</a>
+							</div>
 	          </div>
 	        </div>
 	      </div>
