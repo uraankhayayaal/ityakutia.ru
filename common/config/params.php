5df1371bb3135e0a01485eed815716def2522b4e
uraankhayayaal <uraankhayayaal@gmail.com>
2021-01-07 14:23:12 +0300

    new theme added

--- common/config/params.php
+++ common/config/params.php
@@ -5,4 +5,5 @@ return [
     'managerEmail' => 'manager@admin14.ru',
     // 'supportEmail' => 'manager@admin14.ru',
     'user.passwordResetTokenExpire' => 3600,
+    'domain' => 'https://ityakutia.ru'
 ];
