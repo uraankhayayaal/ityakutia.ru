3cca758e43e9019b8cfb93d9f9caeecbb14c637c
uraankhayayaal <uraankhayayaal@gmail.com>
2021-02-27 23:19:06 +0900

    Map added

--- common/modules/smartlink/views/back/view.php
+++ common/modules/smartlink/views/back/view.php
@@ -10,7 +10,57 @@ use yii\widgets\LinkPager;
 
 $this->title = $model->company . '/' . $model->app_name;
 ?>
+<script src="https://api-maps.yandex.ru/2.1/?apikey=ваш API-ключ&lang=ru_RU" type="text/javascript"></script>
 <div class="banner-view">
+    <div class="row">
+        <div class="col s12">
+            <p></p>
+            <div id="map" style="width: 100%; height: 550px;"></div>
+            <?php
+                $points = Movement::find()->select(['coordinate'])->where(['smartlink_id' => $model->id])->all();
+                $array = [];
+                foreach ($points as $key => $point) {
+                    if (is_double(json_decode($point->coordinate, true)["lat"]) && json_decode($point->coordinate, true)["lng"]){
+                        $array[] = [json_decode($point->coordinate, true)["lat"], json_decode($point->coordinate, true)["lng"]];
+                    }
+                }
+                $coords = json_encode($array);
+                $this->registerJs("
+                    ymaps.ready(function () {  
+                        
+                        var myMap = new ymaps.Map('map', {
+                            center: [62.0397, 129.7422],
+                            zoom: 4
+                        });
+
+                        var coords = $coords;
+
+                        // var myCollection = new ymaps.GeoObjectCollection();
+                        var myGeoObjects = [];
+                        for (var i = 0; i<coords.length; i++) {
+                            // myCollection.add(new ymaps.Placemark(coords[i]));
+                            myGeoObjects[i] = new ymaps.GeoObject({
+                                geometry: {
+                                type: 'Point',
+                                coordinates: coords[i]
+                                }
+                            });
+                        }
+
+                        var myClusterer = new ymaps.Clusterer();
+                        myClusterer.add(myGeoObjects);
+
+                        myMap.geoObjects.add(myClusterer);
+
+                    });
+                
+                ",$this::POS_READY);
+            ?>
+            <script>
+                
+            </script>
+        </div>
+    </div>
     <div class="row">
         <div class="col s12">
             <?= GridView::widget([
