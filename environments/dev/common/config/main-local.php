4a78fd267c07ea63bd27f978d1b64c289ad2d17a
Ayaal Kaplin <uraankhayayaal@gmail.com>
2022-03-14 11:06:24 +0900

    update dev env for docker-compose run

--- environments/dev/common/config/main-local.php
+++ environments/dev/common/config/main-local.php
@@ -3,9 +3,9 @@ return [
     'components' => [
         'db' => [
             'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=localhost;dbname=admin14.loc',
-            'username' => 'admin14.loc',
-            'password' => 'admin14.loc',
+            'dsn' => 'mysql:host=db;dbname=admin14.ru',
+            'username' => 'admin14.ru',
+            'password' => 'admin14.ru',
             'charset' => 'utf8',
         ],
         'mailer' => [
